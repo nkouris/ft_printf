a3989c56fcc1e7c73b95a50b25ac3b6cf4739b4b
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 15:42:08 -0800

    stripping out each part of di conv

--- src/conversion_diou.c
+++ src/conversion_diou.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 13:17:55 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 15:39:14 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 15:41:31 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,11 @@ void		conv_d_i(const char **format, t_flags *flags, va_list *args)
 	{*/
 		num = va_arg(*args, int);
 		lnum = (long long)num;
-//	}
+/*	}
 	if (flags->lenmod[0] == 'h' && flags->lenmod[1] == 'h')
 		lnum = (long long)(char)num;
 	else if (flags->lenmod[0] == 'h' && flags->lenmod[1] != 'h')
-		lnum = (long long)(short)num;
+		lnum = (long long)(short)num;*/
 	relen = count_num_signed(lnum, 10);
 	base_conv_signed(lnum, str, 10, relen);
 	if (flags->spacepad && flags->fieldwidth < relen)
