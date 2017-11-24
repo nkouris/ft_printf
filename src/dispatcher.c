9528ef3feb8950c240bf14ad2e0f4cd8573f27c4
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 13:45:31 -0800

    removed string support entirely, as this may be the culprit

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 15:04:31 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 13:28:56 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 13:45:06 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static void	parse_conv(const char **format, t_flags *flags, va_list *args)
 {
-	if (**format == 's' || **format == 'S')
-		conv_s(format, flags, args);
+/*	if (**format == 's' || **format == 'S')
+		conv_s(format, flags, args);*/
 	else if (**format == 'p')
 		conv_p(flags, args);
 	else if (**format == 'd' || **format == 'D'
