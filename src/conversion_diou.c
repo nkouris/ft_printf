28949579b4da52d091a009c60c1de991e25f1d13
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 13:04:45 -0800

    removed seg fault break

--- src/conversion_diou.c
+++ src/conversion_diou.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 13:17:55 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 12:59:58 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 13:04:23 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,6 @@ void		conv_d_i(const char **format, t_flags *flags, va_list *args)
 		flags->fieldwidth = relen + 1;
 	if (flags->preper && !lnum)
 		relen--;
-	while (num)
-		num++;
 	conv_d_i_u_owrite(flags, str, relen, lnum);
 }
 
