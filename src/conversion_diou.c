81a101cdf66a0396b8af775e836d02969e0c556e
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 15:48:09 -0800

    restored most parts of di conv

--- src/conversion_diou.c
+++ src/conversion_diou.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 13:17:55 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 15:45:24 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 15:47:43 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,20 @@ void		conv_d_i(const char **format, t_flags *flags, va_list *args)
 	long long	lnum;
 
 //	ft_memset(str, 0, 64);
-	/*
+
 	if (flags->lenmod[0] >= 106 || **format == 'D')
 		lnum = va_arg(*args, long long);
 	else 
-	{*/
+	{
 		num = va_arg(*args, int);
 		lnum = (long long)num;
-/*	}
+	}
 	if (flags->lenmod[0] == 'h' && flags->lenmod[1] == 'h')
 		lnum = (long long)(char)num;
 	else if (flags->lenmod[0] == 'h' && flags->lenmod[1] != 'h')
-		lnum = (long long)(short)num;*/
-//	relen = count_num_signed(lnum, 10);
-//	base_conv_signed(lnum, str, 10, relen);
-	base_conv_signed(lnum, str, 10, 10);
+		lnum = (long long)(short)num;
+	relen = count_num_signed(lnum, 10);
+	base_conv_signed(lnum, str, 10, relen);
 	if (flags->spacepad && flags->fieldwidth < relen)
 		flags->fieldwidth = relen + 1;
 	if (flags->preper && !lnum)
