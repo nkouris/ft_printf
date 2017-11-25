008e2f517562528d85b9409729a4343e4908f694
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 17:08:49 -0800

    this is a mystery

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 15:04:31 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 17:06:35 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 17:08:41 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ static void	parse_conv(const char **format, t_flags *flags, va_list *args)
 	else if (**format == 'n')
 		conv_n(flags, args);
 	*/
+	else
+		(*format)--;
 }
 
 static void	clear_flags(t_flags *flags)
