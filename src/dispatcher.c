4564669bbf689a87c37f1f8c3ede61fc96cf56ed
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 22:03:18 -0800

    remove n support

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 15:04:31 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 20:48:41 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 22:01:23 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ static void	parse_conv(const char **format, t_flags *flags, va_list *args)
 		conv_flag(flags);
 	else if (**format == 'c' || **format == 'C')
 		conv_c(flags, args);
-	else if (**format == 'n')
-		conv_n(flags, args);
+/*	else if (**format == 'n')
+		conv_n(flags, args);*/
 	else
 		(*format)--;
 }
