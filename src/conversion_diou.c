d1aece5535a25978f2b707c21b2d0face72ddb6a
Nicholas KOURIS <nkouris@student.42.us.org>
2017-11-24 17:14:46 -0800

    segf in conv_d_i @ D

--- src/conversion_diou.c
+++ src/conversion_diou.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 13:17:55 by nkouris           #+#    #+#             */
-/*   Updated: 2017/11/24 17:12:22 by nkouris          ###   ########.fr       */
+/*   Updated: 2017/11/24 17:14:19 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,11 @@ void		conv_d_i(const char **format, t_flags *flags, va_list *args)
 	long long	lnum;
 
 	ft_memset(str, 0, 64);
+	num = 1;
 	if (flags->lenmod[0] >= 106 || **format == 'D')
+	{
+		while (num)
+			num++;
 		lnum = va_arg(*args, long long);
 	else 
 	{
